dbf08abfbc6188a392e0c52682607fbc18b7a7e2
Audyari Wiyono <audy123ari@gmail.com>
2026-06-20 18:09:43 +0700

    Header Extractor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{Router, extract::Query, routing::get};
+use http::HeaderMap;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -11,9 +12,19 @@ async fn list_items(Query(filter): Query<Filter>) -> String {
     format!("Page: {}, Limit: {}", filter.page, filter.limit)
 }
 
+async fn read_headers(headers: HeaderMap) -> String {
+    let user_agent = headers
+        .get("user-agent")
+        .and_then(|v| v.to_str().ok())
+        .unwrap_or("Unknown");
+    format!("User-Agent: {}", user_agent)
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/items", get(list_items));
+    let app = Router::new()
+        .route("/headers", get(read_headers))
+        .route("/items", get(list_items));
     let listener = tokio::net::TcpListener::bind("localhost:3000")
         .await
         .unwrap();
