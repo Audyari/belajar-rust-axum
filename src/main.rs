6f3765f81d2ea2b16e839a89c96bde06cef33eda
Audyari Wiyono <audy123ari@gmail.com>
2026-06-20 22:29:53 +0700

    Closure Capture

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,22 @@ use axum::{Router, extract::State, routing::get};
 use std::sync::Arc;
 
 struct AppState {
-    counter: u32,
     message: String,
-}
-
-async fn home(State(state): State<Arc<AppState>>) -> String {
-    format!("{} - Counter: {}", state.message, state.counter)
 }
 
 #[tokio::main]
 async fn main() {
     let state = Arc::new(AppState {
-        counter: 10,
-        message: "Hello".to_string(),
+        message: "Hello from closure!".to_string(),
     });
 
-    let app = Router::new().route("/", get(home)).with_state(state);
+    // ✅ PAKAI STATE (paling aman untuk Axum)
+    let app = Router::new()
+        .route(
+            "/",
+            get(|State(state): State<Arc<AppState>>| async move { state.message.clone() }),
+        )
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("localhost:3000")
         .await
