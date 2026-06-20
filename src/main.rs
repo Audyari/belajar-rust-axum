e27c122923ee3afd3e5fc7305b7b26f58afad4b2
Audyari Wiyono <audy123ari@gmail.com>
2026-06-20 18:07:59 +0700

    Query Extractor

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
-use axum::{
-    Json, Router,
-    routing::{get, post},
-};
+use axum::{Router, extract::Query, routing::get};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-struct User {
-    name: String,
-    age: u8,
+struct Filter {
+    page: u32,
+    limit: u32,
 }
 
-async fn create_user(Json(payload): Json<User>) -> String {
-    format!("{} is {} years old", payload.name, payload.age)
-}
-
-async fn hello() -> String {
-    "Hello World".to_string()
+async fn list_items(Query(filter): Query<Filter>) -> String {
+    format!("Page: {}, Limit: {}", filter.page, filter.limit)
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/user", post(create_user))
-        .route("/", get(hello));
-
+    let app = Router::new().route("/items", get(list_items));
     let listener = tokio::net::TcpListener::bind("localhost:3000")
         .await
         .unwrap();
-    println!("🚀 Server running at http://localhost:3000");
     axum::serve(listener, app).await.unwrap();
 }
