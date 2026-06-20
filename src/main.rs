96051b8c1611ed57a46035dc0a044a713bf47274
Audyari Wiyono <audy123ari@gmail.com>
2026-06-20 19:41:51 +0700

    Form Request

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use axum::{Router, http::StatusCode, response::IntoResponse, routing::get};
+use axum::{Form, Router, routing::post};
+use serde::Deserialize;
 
-async fn handler() -> impl IntoResponse {
-    (StatusCode::OK, "Hello World")
+#[derive(Deserialize)]
+#[allow(dead_code)] // Tambahkan attribute ini agar field password tidak dianggap unused
+struct LoginForm {
+    username: String,
+    password: String,
 }
 
-// curl http://localhost:3000/
-// Output: Hello World
+async fn login(Form(form): Form<LoginForm>) -> String {
+    format!("Welcome {}", form.username)
+}
+
+// curl -X POST http://localhost:3000/login -H "Content-Type: application/x-www-form-urlencoded" -d "username=admin&password=password"
+// Output: Welcome admin
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new().route("/login", post(login));
 
     let listener = tokio::net::TcpListener::bind("localhost:3000")
         .await
