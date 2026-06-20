8e943cc0a9d092744f94a66ddd4211264e1f1196
Audyari Wiyono <audy123ari@gmail.com>
2026-06-20 19:09:16 +0700

    Json Extractor

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
-use axum::{Router, routing::post};
+use axum::{Router, extract::Json, routing::post};
+use serde::Deserialize;
 
-async fn echo(body: String) -> String {
-    format!("You said: {}", body)
+#[derive(Deserialize)]
+struct User {
+    name: String,
+    age: u8,
 }
 
-// curl -X POST http://localhost:3000/echo -d "Hello World"
-// Output: You said: Hello World
+async fn create_user(Json(payload): Json<User>) -> String {
+    format!("User: {}, Age: {}", payload.name, payload.age)
+}
+
+// curl -X POST http://localhost:3000/user -H "Content-Type: application/json" -d "{\"name\":\"Alice\",\"age\":30}"
+// Output: User: Alice, Age: 30
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/echo", post(echo));
+    let app = Router::new().route("/user", post(create_user));
     let listener = tokio::net::TcpListener::bind("localhost:3000")
         .await
         .unwrap();
